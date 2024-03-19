cc5d36bec4b604c99fe04b010fade34e5f2ae4b9
Juan J. Blancop <juanjb.dev@gmail.com>
2024-03-18 19:36:42 -0500

    (feat) first commits

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "SEO Title",
+    description: "SEO Description",
+    keywords: ["About Page", "Fernando", "Information", "..."],
+};
+
+export default function AboutPage() {
+    return <span className="text-7xl">About Page</span>;
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Contact Page Title",
+    description: "Contact Page Description",
+    keywords: ["Contact Page", "Juanito Alimaña", "Information", "..."],
+};
+
+export default function PricingPage() {
+    return (
+        <>
+            <span className="text-7xl">Pricing Page</span>
+        </>
+    );
+}
